837c736537717aefb4fae3c9d6a592490cd9e856
Lowell Mower <lowell.m.mower@gmail.com>
2015-08-31 09:05:40 -0400

    hardcode fixtures

--- app/models/todo.js
+++ app/models/todo.js
@@ -3,4 +3,27 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   isComplete: DS.attr('boolean')
+}).reopenClass({
+  FIXTURES: [
+    {
+      id: 1,
+      title: "Make a TODO app in EmberJS",
+      isComplete: false
+    },
+    {
+      id: 2,
+      title: "Compare angular, react, ember to eachother",
+      isComplete: true
+    },
+    {
+      id: 3,
+      title: "Bring clothes to the laundry-mat",
+      isComplete: true
+    },
+    {
+      id: 4,
+      title: "Do the dishes",
+      isComplete: false
+    }
+  ]
 });
